f8a75892f4ff8a33c0c3bb0995b5eac394e0e613
Fran Riadigos <riadigos.fran@gmail.com>
2020-01-01 16:19:12 +0000

    Updated remove sh

--- remove.sh
+++ remove.sh
@@ -5,7 +5,7 @@ submodule="$1"
 
 git submodule deinit "$submodule"
 git rm --cached "$submodule"
-rm -rf "$submodule"
+rm -rf "./$submodule"
 rm -rf ".git/modules/$submodule"
 git config -f .gitmodules --remove-section "submodule.$submodule"
 git config -f .git/config --remove-section "submodule.$submodule"
